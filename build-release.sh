1925a0114eeb8aa9e0b6afeeea513f41bb2b8ab8
nirokay <nirokay-public@protonmail.com>
2025-01-13 11:16:49 +0100

    typo fix

--- build-release.sh
+++ build-release.sh
@@ -10,4 +10,4 @@ fi
 ARCH="_$ARCH"
 nimble build -d:release && mv gitman "gitman_${OSTYPE}${ARCH}"
 
-nimble build -d:mingw -d:windows -d:release && mv gitman.exe "gitman_windows_${ARCH}.exe"
+nimble build -d:mingw -d:windows -d:release && mv gitman.exe "gitman_windows${ARCH}.exe"
